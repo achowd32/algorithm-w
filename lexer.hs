930559f0064ff53f7b38d8a4192f85225269fdc0
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2026-09-22 23:13:26 -0400

    basic parser

--- lexer.hs
+++ lexer.hs
@@ -2,9 +2,23 @@ import System.IO
 import System.Environment
 import Text.Read
 
-data Literal = Num Int | Boolean Bool
-data Op = PLUS | MINUS
-data Token = TokLit Literal | TokOp Op | Invalid
+data Literal = Num Int | Boolean Bool deriving (Show)
+data Op = PLUS | MINUS | OR | AND deriving (Show)
+
+data Token = TokLit Literal | TokOp Op | Invalid deriving (Show)
+
+data Node
+    = Lit Literal
+    | BinOp Op Node Node
+    | Bad
+    deriving (Show)
+
+parse :: [Token] -> Node
+parse [] = Bad
+parse (Invalid : _) = Bad
+parse [TokLit n] = Lit n
+parse (TokLit n : TokOp o : rest) = BinOp o (Lit n) (parse rest)
+parse _ = Bad
 
 lexOne :: String -> Token
 lexOne str = 
@@ -15,6 +29,8 @@ lexOne str =
                "false" -> TokLit (Boolean False)
                "-" -> TokOp MINUS
                "+" -> TokOp PLUS
+               "|" -> TokOp OR
+               "&" -> TokOp AND
                _ -> Invalid
 
 lexAll :: [String] -> [Token]
@@ -24,5 +40,5 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [s] -> lexAll (words s)
+        [s] -> print (parse (lexAll (words s)))
         _ -> putStrLn "wrong"
